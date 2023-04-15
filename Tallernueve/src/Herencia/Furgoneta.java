57b308beb676ffa8e1c896709501ff14a5f185fe
luisposso <lumapo1825@gmail.com>
2023-04-14 20:12:03 -0500

    taller 9

---
+++ Tallernueve/src/Herencia/Furgoneta.java
@@ -0,0 +1,14 @@
+package Herencia;
+
+
+public class Furgoneta extends Coche{
+	private int capacidadCarga;
+	private int plazasExtras;
+	
+	public Furgoneta(int plazasExtras, int capacidadCarga) {
+		super();
+		
+		this.capacidadCarga = capacidadCarga;
+		this.plazasExtras = plazasExtras;
+	}
+}
